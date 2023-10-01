0ed10580983190581a4670b79cdc34c0ba0bdf1e
LuthfiPhysio <136604472+LuthfiPhysio@users.noreply.github.com>
2023-10-01 17:02:54 +0700

    Install library and edit config app

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        \Srmklive\PayPal\Providers\PayPalServiceProvider::class,
     ])->toArray(),
 
     /*
@@ -182,7 +183,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
+        'PayPal' => \Srmklive\PayPal\Facades\PayPal::class,
     ])->toArray(),
 
 ];
